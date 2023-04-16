0eb011b2dbd0b4c7806e589c83e21fb49f9fc59b
Jim Fisher <jameshfisher@gmail.com>
2023-04-16 13:32:21 +0100

    recurse - buggy

--- agent.ts
+++ agent.ts
@@ -1,72 +1,102 @@
 import { VM } from "vm2";
+import { OpenAIChat } from "./llms/openai.js";
 import { GoogleSearchTool } from "./tools/google.js";
-import { LLM } from "./types.js";
 
 const PROMPT_TEMPLATE = ({
-  notes,
+  task,
 }: {
-  notes: unknown;
-}) => `You are an assistant.
-Your working memory is in a JS object called 'notes'.
-Your job is to answer the question in notes.question,
-and put the answer in notes.answer.
-You work step-by-step by choosing JS actions from the following:
+  task: unknown;
+}) => `An agent is an async JS function whose body is dynamically written by AI when invoked, e.g.:
 
-  notes[<string>] = <string>;
-  notes[<string>] = await google(query: string);
-  delete notes[<string>];
+async agent(task: "What is ninety-four as a number?") { return 94; }
 
-Here are some excellent examples.
+When an agent can answer with a simple calculation, it can use a JS expression, e.g.:
 
-notes = { "question": "What is 94-56?" };
-notes.answer = 94-56; // Use JS to calculate this
+async agent(task: "What is two times 94 as a string?") { return (2 * 94).toString(); }
 
-notes = { "question": "How old was QEII's father when he died?" };
-notes["plan"] = "Find QEII's father, then find his age at death"; // Multi-step question. Write down steps
+The task may be a JSON object, e.g.:
 
-notes = { "question": "How old is the world's most popular programming language?", "most popular programming language": "JavaScript" };
-notes["javascript invention date"] = await google("When was JavaScript invented?"); // We've found the language, but need to find its age
+async agent(task: { "question": "What is the second item in foo?", foo: [42, 3.14, null] }) {
+  return task.foo[1];
+}
 
-notes = { "question": "How old was QEII's father when he died?", "plan": "Find QEII's father, then find his age at death" };
-notes["qeii's father"] = await google("What was QEII's father's name?"); // Do first step of plan
+If the task requires external simple facts, the agent can use \`google\`, e.g.:
 
-notes = { "question": "How old is the world's most popular programming language?", "most popular programming language": "JavaScript", "javascript invention year": "1995", "current year": "2023" };
-notes["answer"] = (2023 - 1995) + " years"; // Calculate from numbers in notes
+async agent(task: "When was JavaScript invented?") {
+  return google(task);
+}
 
-notes = ${JSON.stringify(notes)};
-`;
+If the task requires sequential steps, the agent can invoke sub-agents, e.g.:
 
-const MAX_LOOPS = 5;
+async agent (task: "How old was QEII's father when he died?") {
+  return agent({ task, "QEII's father's name was": await google("What was QEII's father's name?") });
+}
 
-export const runAgent = async ({
-  question,
-  llm,
-}: {
-  question: string;
-  llm: LLM;
-}): Promise<string> => {
+The task may contain useful facts and planning, e.g.:
+
+async agent(task: { "question": "How old is the world's most popular programming language?", "most popular programming language": "JavaScript", "javascript invention year": "1995", "current year": "2023" }) {
+  return \`${2023 - 1995} years\`;
+}
+
+The agent may do async tasks concurrently, e.g.
+
+async agent(task: "What is the difference in years between the ages of Noam Chomsky and David Cameron?") {
+  const [chomskyAge, cameronAge] = await Promise.all(
+    google("How old is Noam Chomsky?"),
+    google("How old is David Cameron?"),
+  ]);
+  return agent({ task, chomskyAge, cameronAge });
+}
+
+If the task requires a plan, the agent can write one, then call itself recursively:
+
+async agent(task: "How old was QEII's father when he died?") {
+  const plan = "Find QEII's father, then find his age at death";
+  return agent({ task, plan });
+}
+
+Now here's an excellent example of an agent:
+
+async agent(task: ${JSON.stringify(task)}) {`;
+
+const llm = OpenAIChat;
+
+let budget = 7;
+
+const decr = () => {
+  budget--;
+  if (budget <= 0) {
+    throw new Error("Budget exceeded");
+  }
+};
+
+export const agent = async (task: unknown): Promise<unknown> => {
+  decr();
   const vm = new VM({
     timeout: 5000,
     allowAsync: true,
     sandbox: {
-      notes: { question },
-      google: GoogleSearchTool.use,
+      task,
+      google: async (q: unknown) => {
+        decr();
+        if (typeof q !== "string") throw new Error("google() expects a string");
+        console.log("google", q);
+        const res = await GoogleSearchTool.use(q);
+        console.log("google res", res);
+        return res;
+      },
+      agent, // recursive call
     },
   });
 
-  for (let numLoops = 0; numLoops < MAX_LOOPS; numLoops += 1) {
-    const prompt = PROMPT_TEMPLATE({ notes: vm.sandbox.notes });
-    const jsAction = await llm(prompt, [";"]);
-    console.log({ jsAction });
-    await vm.run(`(async () => {
-      ${jsAction};
-    })()`);
-    console.log("notes after", vm.sandbox.notes);
-    if (vm.sandbox.notes.answer) {
-      console.log({ "FINAL ANSWER": vm.sandbox.notes.answer });
-      return vm.sandbox.notes.answer;
-    }
-  }
-
-  throw new Error(`Max loops reached: ${MAX_LOOPS}`);
+  const prompt = PROMPT_TEMPLATE({ task: vm.sandbox.task });
+  console.log({ prompt });
+  const jsFnBody = await llm(prompt, ["}"]);
+  console.log({ jsFnBody });
+  const answer = await vm.run(`(async () => {
+    const task = ${JSON.stringify(task)};
+    ${jsFnBody}
+  })()`);
+  console.log({ answer });
+  return answer;
 };
--- examples/runAgent.ts
+++ examples/runAgent.ts
@@ -1,11 +1,9 @@
-import { runAgent } from "../agent.js";
+import { agent } from "../agent.js";
 import { OpenAIChat } from "../llms/openai.js";
 import { JavaScriptEvaluatorTool } from "../tools/javascript.js";
 
-const result = await runAgent({
-  llm: OpenAIChat,
-  question:
-    "What is the difference in years between the ages of Noam Chomsky and David Cameron?",
-});
+const result = await agent(
+  "What was the value of the NASDAQ this week, plus the value of the S&P 500 this week, as a number?"
+);
 
 console.log(result);
